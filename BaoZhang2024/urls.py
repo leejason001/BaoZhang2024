a4def29cfe986f98300d840dffdb0ef8eba05fd2
leejason <13131412952@163.com>
2024-03-08 10:17:05 +0800

    完成项目目录结构

--- BaoZhang2024/urls.py
+++ BaoZhang2024/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'site/', include('mySite.urls')),
+    url(r'backend/', include('backend.urls')),
 ]
---
+++ backend/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def backendIndex(request):
+    return HttpResponse("bbbbbbbbbbbbb")
