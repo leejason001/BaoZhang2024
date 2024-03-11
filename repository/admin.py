26456c62d1e24d0daf926da983a8b51454ba76aa
leejason <13131412952@163.com>
2024-03-11 17:12:16 +0800

    增加可能无用的文件

---
+++ repository/admin.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+# Register your models here.
+from repository import models
+admin.site.register(models.users)
+admin.site.register(models.blogs)
+admin.site.register(models.reportTroubles)
+admin.site.register(models.classifications)
+admin.site.register(models.articlesDetail)
+admin.site.register(models.articles)
+admin.site.register(models.labels)
+admin.site.register(models.labelArticleRelationShip)
+admin.site.register(models.readerAttitude)
+admin.site.register(models.comments)
