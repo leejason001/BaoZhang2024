c683d6080cc9cf226f07871aa570197204d6bc86
leejason <13131412952@163.com>
2024-03-24 16:42:01 +0800

    搭好主站页框架

--- mySite/views/home.py
+++ mySite/views/home.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.shortcuts import HttpResponse, redirect, render
 
 def index(request, *args, **kwargs):
-    print kwargs
-    return HttpResponse(kwargs)
+
+    return render(request, 'mySite/home.html', {'themeCS_Navigator':''})
