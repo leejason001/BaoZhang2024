b1f7bd9aa44a83545004c504be04d00df4a37fcd
leejason <13131412952@163.com>
2024-03-26 16:16:29 +0800

    实现“标签”视图

--- mySite/urls.py
+++ mySite/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     url(r'mySite/login.html', realViews.doLogin, name='login'),
     url(r'mySite/getCheckcode', realViews.getValidateCodeImage),
     url(r'mySite/logout', realViews.doLogout, name='logout'),
+    url(r'(.+)\/label\/(?P<label_id>\d)\.html', home.theLabelArticles),
     url(r'(.+)\/(?P<artilce_id>\d+)\.html', home.wholeArticle ),
     url(r'^(?P<surfix>.+)\.html$', home.index),
     url(r'^$', realViews.index),
--- mySite/views/home.py
+++ mySite/views/home.py
@@ -23,9 +23,28 @@ def index(request, *args, **kwargs):
         theLabels.append(label)
 
 
-    return render(request, 'mySite/home.html', {'theBlog':theBlog, 'articles':articles[startItemNum:endItemNum], 'paginations':mark_safe(paginations),'themeCS_Navigator':'', 'labels':theLabels, 'classifications': theBlog.classifications_set.all()})
+    return render(request, 'mySite/home.html', {'theBlog':theBlog, 'articles':articles[startItemNum:endItemNum], 'paginations':mark_safe(paginations),'themeCS_Navigator':'', 'labels': theLabels, 'classifications': theBlog.classifications_set.all()})
 
 def wholeArticle(request, *args, **kwargs):
     theBlog = models.blogs.objects.filter( owner_id=request.session['id_login'] ).first()
     article = models.articles.objects.filter(id=kwargs['artilce_id']).first()
     return render(request, 'mySite/article.html', {'theBlog':theBlog, 'article':article, 'labels':theBlog.labels_set.all()})
+
+def theLabelArticles(request, *args, **kwargs):
+    theBlog = models.blogs.objects.filter(owner_id=request.session['id_login']).first()
+    articles    = models.articles.objects.filter(labelarticlerelationship__label__id=kwargs['label_id'])
+
+    paginationHrefPrefix = '/' + theBlog.surfix + '/label/' + kwargs['label_id'] + '.html'
+    try:
+        currentPageNum         = int(request.GET.get('currentPageNum'))
+    except:
+        currentPageNum         = 1
+    paginations, startItemNum, endItemNum = pagination.returnPaginations( currentPageNum, articles.count(),
+                                                                          paginationHrefPrefix )
+
+    theLabels = []
+    for label in theBlog.labels_set.all():
+        label.count = models.labelArticleRelationShip.objects.filter(label_id=label.id).count()
+        theLabels.append(label)
+
+    return render(request, 'mySite/home.html', {'theBlog':theBlog, 'articles':articles[startItemNum:endItemNum], 'paginations':mark_safe(paginations), 'labels': theLabels, 'classifications': theBlog.classifications_set.all()})
