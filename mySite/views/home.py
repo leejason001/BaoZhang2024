d0aa573fa585a27dc1f28661f512766fcb421e45
leejason <13131412952@163.com>
2024-03-28 10:19:55 +0800

    优化代码

--- mySite/views/home.py
+++ mySite/views/home.py
@@ -9,7 +9,7 @@ from utils import pagination
 
 def index(request, *args, **kwargs):
     blogAndUserTable = models.blogs.objects.all().select_related('owner')
-    
+
     theBlogAndUser = blogAndUserTable.filter(surfix=kwargs['surfix']).first()
     articles= models.articles.objects.filter(ownerBlog_id=theBlogAndUser.id)
     paginationHrefPrefix = '/' + theBlogAndUser.surfix + '.html'
@@ -28,7 +28,7 @@ def index(request, *args, **kwargs):
     return render(request, 'mySite/home.html', {'user':theBlogAndUser,'theBlog':theBlogAndUser, 'articles':articles[startItemNum:endItemNum], 'paginations':mark_safe(paginations),'themeCS_Navigator':'', 'labels': theLabels})
 
 def wholeArticle(request, *args, **kwargs):
-    theBlog = models.blogs.objects.filter( owner_id=request.session['id_login'] ).first()
+    theBlog = models.blogs.objects.filter( surfix=kwargs['surfix'] ).first()
     article = models.articles.objects.filter(id=kwargs['artilce_id']).first()
 
     theLabels = []
@@ -38,7 +38,7 @@ def wholeArticle(request, *args, **kwargs):
     return render(request, 'mySite/article.html', {'theBlog':theBlog, 'article':article, 'labels':theLabels})
 
 def theLabelArticles(request, *args, **kwargs):
-    theBlog = models.blogs.objects.filter(owner_id=request.session['id_login']).first()
+    theBlog = models.blogs.objects.filter( surfix=kwargs['surfix'] ).first()
     articles    = models.articles.objects.filter(labelarticlerelationship__label__id=kwargs['label_id'])
 
     paginationHrefPrefix = '/' + theBlog.surfix + '/label/' + kwargs['label_id'] + '.html'
