cc5c5827164a4a27062ba33ecc87b378d06511bb
leejason <13131412952@163.com>
2024-03-28 17:19:24 +0800

    有的时候可能必须用extra,raw不行

--- mySite/urls.py
+++ mySite/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     url(r'mySite/login.html', realViews.doLogin, name='login'),
     url(r'mySite/getCheckcode', realViews.getValidateCodeImage),
     url(r'mySite/logout', realViews.doLogout, name='logout'),
+    url(r'^(?P<surfix>\w+)\/date\/(?P<theDate>\d{4}-\d{2})\.html', home.theDateArticles),
     url(r'(?P<surfix>\w+)\/label\/(?P<label_id>\d)\.html', home.theLabelArticles),
     url(r'(?P<surfix>\w+)\/(?P<artilce_id>\d+)\.html', home.wholeArticle ),
     url(r'^(?P<surfix>\w+)\.html$', home.index),
--- mySite/views/home.py
+++ mySite/views/home.py
@@ -57,3 +57,19 @@ def theLabelArticles(request, *args, **kwargs):
         theLabels.append(label)
 
     return render(request, 'mySite/home.html', {'theBlog':theBlog, 'articles':articles[startItemNum:endItemNum], 'paginations':mark_safe(paginations), 'labels': theLabels})
+
+def theDateArticles(request, *args, **kwargs):
+    theBlog = models.blogs.objects.filter(surfix=kwargs['surfix']).first()
+    articles = models.articles.objects.filter( ownerBlog=theBlog ).extra(
+        where=['strftime("%%Y-%%m",ctime)=%s'], params=[kwargs['theDate'], ] ).all()
+    print list(articles)
+
+    paginationHrefPrefix = '/' + theBlog.surfix + '/date/' + kwargs['theDate'] + '.html'
+    try:
+        currentPageNum         = int(request.GET.get('currentPageNum'))
+    except:
+        currentPageNum         = 1
+    paginations, startItemNum, endItemNum = pagination.returnPaginations( currentPageNum, len(list(articles)),
+                                                                          paginationHrefPrefix )
+
+    return render(request, 'mySite/home.html', {'theBlog':theBlog, 'articles':articles[startItemNum:endItemNum], 'paginations':mark_safe(paginations)})
