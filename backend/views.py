9c400fa7febc575051751ce1587c820a398d1e2d
leejason <13131412952@163.com>
2024-04-25 17:39:46 +0800

    完成bodyRight框架

--- backend/views.py
+++ backend/views.py
@@ -6,4 +6,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def articleManager(request, tabs):
-    return render(request, 'backend/backendBase.html', {'tabs': tabs})
+    return render(request, 'backend/articleManager.html', {'tabs': tabs, 'theTabCaption': u'文章管理', 'crumbs': [u'文章列表']})
