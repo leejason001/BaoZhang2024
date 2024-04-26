fd0e04011d8007ba93f573ade187ff7d33e494a3
leejason <13131412952@163.com>
2024-04-26 15:00:25 +0800

    为实现“组合搜索”做好准备工作

--- backend/views.py
+++ backend/views.py
@@ -3,7 +3,15 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse
 
+from repository import models
 # Create your views here.
 
 def articleManager(request, tabs):
-    return render(request, 'backend/articleManager.html', {'tabs': tabs, 'theTabCaption': u'文章管理', 'crumbs': [u'文章列表']})
+    try:
+        theBlog = models.blogs.objects.filter(owner_id=request.session['id_login'])
+        classifications = models.classifications.objects.filter(owner=theBlog)
+        labels          = models.labels.objects.filter(toBlog=theBlog)
+        articles        = models.articles.objects.filter(ownerBlog=theBlog)
+        return render(request, 'backend/articleManager.html', {'tabs': tabs, 'theTabCaption': u'文章管理', 'crumbs': [u'文章列表'], 'classifications': classifications, 'labels':labels, 'articles': articles})
+    except:
+        return HttpResponse(u'请先登录')
