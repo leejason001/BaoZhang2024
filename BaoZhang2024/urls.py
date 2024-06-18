135c8cfad24f871903f36b80bb5f853293b97aff
leejason <13131412952@163.com>
2024-06-18 16:16:15 +0800

    调整urls.py中url的顺序，保证兜底在最后，特定url优先匹配

--- BaoZhang2024/urls.py
+++ BaoZhang2024/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url( r'backend/', include( 'backend.urls' ) ),
     url(r'^', include('mySite.urls')),
-    url(r'backend/', include('backend.urls')),
 ]
