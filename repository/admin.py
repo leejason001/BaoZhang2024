0b10587cf0c496d8eb2adce9bf447f27a54b7425
leejason <13131412952@163.com>
2024-06-24 16:20:14 +0800

    把trouble表格注册到admin

--- repository/admin.py
+++ repository/admin.py
@@ -6,20 +6,19 @@ from django.contrib import admin
 # Register your models here.
 from repository import models
 admin.site.register(models.users)
-
 class blogsAdmin(admin.ModelAdmin):
     list_display = ('__str__',)
 admin.site.register(models.blogs, blogsAdmin)
-
 admin.site.register(models.reportTroubles)
 admin.site.register(models.classifications)
 admin.site.register(models.articlesDetail)
-
 class articlesAdmin(admin.ModelAdmin):
     list_display = ('title',)
 admin.site.register(models.articles, articlesAdmin)
-
 admin.site.register(models.labels)
 admin.site.register(models.labelArticleRelationShip)
 admin.site.register(models.readerAttitude)
 admin.site.register(models.comments)
+
+admin.site.register(models.troubles)
+admin.site.register(models.troubleDetail)
