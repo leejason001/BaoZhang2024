b23136aacb319b32c336debae2bced9db7dda847
leejason <13131412952@163.com>
2024-06-24 16:16:24 +0800

    将views.py换成views包

--- backend/urls.py
+++ backend/urls.py
@@ -1,11 +1,13 @@
 #coding:utf8
 
 from django.conf.urls import url
-import views
+from .views import backendManage
+from .views import troubleManage
 
 tabs = [{'path':'11111', 'caption':u'文章管理'}, {'path':'22222', 'caption':u'分类管理'}, {'path':'333333333', 'caption':u'标签管理'}, {'path':'444444', 'caption':u'个人信息'}]
 urlpatterns = [
-    url( r'^createArticle.html$', views.createArticle, {'tabs': tabs}, ),
-    url( r'^editArticle/(?P<article_id>\d+).html$', views.editArticle, {'tabs': tabs}),
-    url(r'(?P<one>\d*)/?(?P<two>\d*)/?', views.articleManager, {'tabs': tabs},),
+    url( r'^createArticle.html$', backendManage.createArticle, {'tabs': tabs}, ),
+    url( r'^editArticle/(?P<article_id>\d+).html$', backendManage.editArticle, {'tabs': tabs}),
+    url(r'^(?P<one>\d*)/?(?P<two>\d*)/?$', backendManage.articleManager, {'tabs': tabs},),
+    url(r'trouble/showTrouble.html', troubleManage.showTroubleList, {'tabs': tabs}),
 ]
