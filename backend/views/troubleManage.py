c740f80bb6c58988c702d22ed3b7f9818f3774fe
leejason <13131412952@163.com>
2024-06-25 15:08:06 +0800

    已接单，就不能让修改了

--- backend/views/troubleManage.py
+++ backend/views/troubleManage.py
@@ -39,17 +39,20 @@ def editTrouble(request, nid, tabs):
     elif 'POST' == request.method:
         editTroubleForm = myForms.TroubleMaker(request.POST)
         if editTroubleForm.is_valid():
-            theTrouble       = models.troubles.objects.filter(id=nid)
-            theTroubleDetail = models.troubleDetail.objects.filter(id=theTrouble[0].detail.id)
-            theTroubleDetail.update(detailContent = editTroubleForm.cleaned_data.pop('detail'))
-            editTroubleForm.cleaned_data.update({'detail':theTroubleDetail})
-            theTrouble.update(
-                title = editTroubleForm.cleaned_data['title'],
-                summary = editTroubleForm.cleaned_data['summary'],
-                detail = theTroubleDetail[0]
-            )
+            theTrouble       = models.troubles.objects.filter(id=nid, status=0)
+            if theTrouble:
+                theTroubleDetail = models.troubleDetail.objects.filter( id=theTrouble[0].detail.id )
+                theTroubleDetail.update( detailContent=editTroubleForm.cleaned_data.pop( 'detail' ) )
+                editTroubleForm.cleaned_data.update( {'detail': theTroubleDetail} )
+                theTrouble.update(
+                    title=editTroubleForm.cleaned_data['title'],
+                    summary=editTroubleForm.cleaned_data['summary'],
+                    detail=theTroubleDetail[0]
+                )
 
-            return redirect('/backend/trouble/showTrouble.html')
+                return redirect( '/backend/trouble/showTrouble.html' )
+            else:
+                return HttpResponse(u"已接单，不能修改")
         else:
             return render(request, 'backend/trouble_editTrouble.html', {'tabs': tabs, 'trouble_id': nid,
                                                                         'troubleEditForm': myForms.TroubleMaker(request.POST)})
